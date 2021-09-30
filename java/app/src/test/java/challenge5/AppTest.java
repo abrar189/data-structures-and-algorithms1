c74e26147b700ab69df283b5d1ee89a0d45e1c96
abrar189 <algourabrar@gmail.com>
2021-09-30 21:07:53 +0300

    done

--- java/app/src/test/java/challenge5/AppTest.java
+++ java/app/src/test/java/challenge5/AppTest.java
@@ -8,50 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+      App classUnderTest = new App();
+      assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
 
-      //Can successfully instantiate an empty linked list
-
-      LinkedList linkedTest = new LinkedList();
-      assertNull(linkedTest.head);
-
-      // Can properly insert into the linked list
-
-      linkedTest.insert(5);
-      assertEquals(5,linkedTest.head.value);
-
-      //The head property will properly point to the first node in the linked list
-
-      assertEquals(5,linkedTest.head.value);
-      assertNull(linkedTest.head.next);
-
-      // Can properly insert multiple nodes into the linked list
-      linkedTest.insert(5);
-      linkedTest.insert(2);
-
-      assertEquals(2,linkedTest.head.value);
-
-      //Will return true when finding a value within the linked list that exists
-
-      linkedTest.insert(5);
-      linkedTest.insert(2);
-      assertTrue(linkedTest.includes(2));
-
-
-
-      //Will return false when searching for a value in the linked list that does not exist
-      linkedTest.insert(5);
-      linkedTest.insert(2);
-      assertFalse(linkedTest.includes(7));
-
-//        System.out.print(linkedTest.toString());
-
-      //Can properly return a collection of all the values that exist in the linked list
-      linkedTest.insert(3);
-      linkedTest.insert(4);
-      assertEquals("{ 4 } -> { 3 } -> { 2 } -> { 5 } -> { 2 } -> { 5 } -> { 2 } -> { 5 } -> { 5 } -> NULL",linkedTest.toString());
-    }
-    }
+    }}
 
 
