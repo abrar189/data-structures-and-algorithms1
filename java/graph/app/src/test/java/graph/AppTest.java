5d941d47550ec079570776964d48cf8956154d70
Abrar <algourabrar@gmail.com>
2021-11-14 20:29:05 +0200

    done

--- java/graph/app/src/test/java/graph/AppTest.java
+++ java/graph/app/src/test/java/graph/AppTest.java
@@ -40,4 +40,25 @@ class AppTest {
 
   }
 
+  @Test
+  public void breadth_First(){
+    Graph graph = new Graph();
+    graph.addNode("10");
+    graph.addNode("5");
+    graph.addNode("1");
+    graph.addNode("4");
+    graph.addNode("7");
+    graph.addNode("6");
+
+    graph.addEdge("10" , "1");
+    graph.addEdge("1" , "5");
+    graph.addEdge("7" , "10");
+    graph.addEdge("4" , "5");
+    graph.addEdge("4" , "6");
+    graph.addEdge("7" , "6");
+
+    assertEquals("[5, 1, 4, 10, 6, 7]",graph.breadthFirst("5").toString());
+
+  }
+
 }
